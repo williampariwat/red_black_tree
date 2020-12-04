b1cd57ecce0d1b11bbd16ae2f47416c8e5bf81b9
williampariwat <william.pariwat@gmail.com>
2020-12-04 16:31:37 +0700

    Adding constructors

--- src/main/scala/red_black_tree.scala
+++ src/main/scala/red_black_tree.scala
@@ -1,3 +1,15 @@
+import java.awt.Color
+
 object red_black_tree {
+  //Setting up classes for Color and Tree
+  sealed abstract class Color
+  case class Red() extends Color
+  case class Black() extends Color
+
+  sealed abstract class Tree[T] //This is the constructor of Tree
+
+  case class Node[T](color: Color, left: Tree[T], value : T, right : Tree[T] ) extends Tree[T]
+  case class Leaf[T]() extends Tree[T] //This acts as "Leaf" in tree
+
 
 }
